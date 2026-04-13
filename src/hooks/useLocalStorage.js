2f166bc194ed54004a1d55ce8efa6c6a456cf1d2
Mads Pedersen <im@tinyleed.com>
2026-04-13 15:57:30 +0200

    Create useLocalStorage.js

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,38 @@
+import { useState, useEffect } from 'react';
+
+function useLocalStorage(key, initialValue) {
+    const [storedValue, setStoredValue] = useState(() => {
+        try {
+            const item = window.localStorage.getItem(key);
+            return item ? JSON.parse(item) : initialValue;
+        } catch (error) {
+            console.error(error);
+            return initialValue;
+        }
+    });
+
+    const setValue = (value) => {
+        try {
+            const valueToStore = value instanceof Function ? value(storedValue) : value;
+            setStoredValue(valueToStore);
+            window.localStorage.setItem(key, JSON.stringify(valueToStore));
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        try {
+            const item = window.localStorage.getItem(key);
+            if (item) {
+                setStoredValue(JSON.parse(item));
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }, [key]);
+
+    return [storedValue, setValue];
+}
+
+export default useLocalStorage;
